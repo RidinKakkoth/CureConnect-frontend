2d3e0175c74a0d8949b0f14af49a2cc53c7bf4e1
Ridin Kakkoth <k.ridin@ymail.com>
2024-10-05 14:33:02 +0530

    book appointment banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Banner from "../components/Banner"
 import Header from "../components/Header"
 import Speciality from "../components/Speciality"
 import TopDoctors from "../components/TopDoctors"
@@ -8,6 +9,7 @@ const Home = () => {
         <Header/>
         <Speciality/>
         <TopDoctors/>
+        <Banner/>
     </div>
   )
 }
