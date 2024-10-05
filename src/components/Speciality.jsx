9a27217e34725b3b61d591f864925737bb92cb1d
Ridin Kakkoth <k.ridin@ymail.com>
2024-10-05 18:28:21 +0530

    all doctors

--- src/components/Speciality.jsx
+++ src/components/Speciality.jsx
@@ -9,7 +9,7 @@ const Speciality = () => {
         <div className="flex gap-4 sm:justify-center pt-5 w-full overflow-scroll">
             {
                 specialityData.map((item,index)=>(
-                    <Link onClick={()=>scrollTo(0,0)} className="flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500" key={index} to={`/doctors/${item.speciality}`}>
+                    <Link onClick={()=>scrollTo(0,0)} to={`/doctors/${item.speciality}`} className="flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500" key={index} >
                         <img className="w-16 sm:w-24 mb-2" src={item.image} alt="" />
                         <p>{item.speciality}</p>
                     </Link>
