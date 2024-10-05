4fd86bbe0d082cd0d6ed749aa3341688fd48c2f7
Ridin Kakkoth <k.ridin@ymail.com>
2024-10-05 12:22:42 +0530

    speciality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header"
+import Speciality from "../components/Speciality"
 
 const Home = () => {
   return (
     <div>
         <Header/>
+        <Speciality/>
     </div>
   )
 }
